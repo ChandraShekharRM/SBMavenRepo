9aaa86d86c1c92fe46238259ac0718af6d7690b9
ChoudaReddy <choudy529@gmail.com>
2022-06-06 22:19:38 +0530

    Added my project

---
+++ SampleSBMaven/src/main/java/com/chouda/SampleSBMaven/SampleSbMavenApplication.java
@@ -0,0 +1,13 @@
+package com.chouda.SampleSBMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSbMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSbMavenApplication.class, args);
+	}
+
+}
---
+++ SampleSBMaven/src/test/java/com/chouda/SampleSBMaven/SampleSbMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chouda.SampleSBMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSbMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
